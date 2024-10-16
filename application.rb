1f1ed8396cef098baa39466deb2da5ee2279b123
KOTP <keeperotphones@gmail.com>
2024-10-16 11:31:25 -0400

    Style consistency
    - Use $stdout instead of the constant, it allows us to temporarily
      change the location of standard out.
    - Remove `progname` variable, as it was not used.
    - Single quote consistency

--- application.rb
+++ application.rb
@@ -1,33 +1,36 @@
 require 'logger'
 
 class ApplicationLogger
+
   LOG_LEVELS = {
-    "DEBUG" => Logger::DEBUG,
-    "INFO"  => Logger::INFO,
-    "WARN"  => Logger::WARN,
-    "ERROR" => Logger::ERROR
+    'DEBUG' => Logger::DEBUG,
+    'INFO'  => Logger::INFO,
+    'WARN'  => Logger::WARN,
+    'ERROR' => Logger::ERROR
   }
 
   def self.build_logger
     # Default to INFO level if environment variable is not set
-    log_level_str = ENV['LOG_LEVEL'] || 'INFO'
-    log_level = LOG_LEVELS.fetch(log_level_str, Logger::INFO)
+    log_level = LOG_LEVELS.fetch(ENV['LOG_LEVEL'], Logger::INFO)
 
-    logger = Logger.new(STDOUT)
+    logger = Logger.new($stdout)
     logger.level = log_level
 
     # Custom log format (optional)
-    logger.formatter = proc do |severity, datetime, progname, msg|
+    logger.formatter = proc do |severity, datetime, msg|
       "#{datetime} - #{severity}: #{msg}\\n"
     end
     logger
   end
+
 end
 
-# Example of using the logger
-logger = ApplicationLogger.build_logger
+if $PROGRAM_NAME == __FILE__
+  # Example of using the logger
+  logger = ApplicationLogger.build_logger
 
-logger.debug("This is a DEBUG message")
-logger.info("This is an INFO message")
-logger.warn("This is a WARN message")
-logger.error("This is an ERROR message")
+  logger.debug("This is a DEBUG message")
+  logger.info("This is an INFO message")
+  logger.warn("This is a WARN message")
+  logger.error("This is an ERROR message")
+end
